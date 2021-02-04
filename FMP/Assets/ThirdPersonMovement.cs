86e280d2748991a4dd20fb3761d8955af75d9a90
a1mftw <hugo.filipereis@hotmail.com>
2021-02-04 21:17:00 +0000

    3rdPersonController + Animations

---
+++ FMP/Assets/ThirdPersonMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public Transform Camera;
+    public CharacterController controller;
+
+    public float turnSmoothTime = 0.1f;
+    public float speed = 6f;
+    public float gravity = 2f;
+    float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        
+        if (direction.magnitude >= 0.1f) 
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + Camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            if (!controller.isGrounded)
+            {
+                moveDirection += Physics.gravity;
+            }
+            
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+
+        }
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Debug.Log("change camera");
+        }
+    }
+
+
+    void CombatTransition() 
+    {
+    
+    
+    }
+
+}
---
+++ FMP/Assets/TransitionController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionController : MonoBehaviour
+{
+
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public void StartGame() 
+    {
+
+        StartCoroutine(StartGameTransition());
+    
+    }
+
+    IEnumerator StartGameTransition() 
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+    
+
+    }
+
+
+
+
+}
