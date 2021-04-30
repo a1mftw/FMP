9868bc6021a169fa40d25d79ce0779b277994801
Hugo <hugo.filipereis@hotmail.com>
2021-04-30 22:55:34 +0100

    options menu

---
+++ FMP/Assets/optionScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class optionScript : MonoBehaviour
+{
+    public AudioSource BGMSource;
+    public Slider BGM;
+    public Slider SFX;
+    float sfxValue = 0.1f;
+    public GameObject optionScreen;
+    public Button Selectable;
+    void Start()
+    {
+        BGM.value = BGMSource.volume;
+        SFX.value = sfxValue;
+    }
+
+    public void ChangeBGMVolume() 
+    {
+        BGMSource.volume = BGM.value;
+    }
+
+    public void ChangeSFXVolume() 
+    {
+
+        sfxValue = SFX.value;
+
+    }
+
+    public void OpenOptions() 
+    {
+        SFX_Manager_HR.instance.PlaySFX(SFX_Manager_HR.SoundEffectNames.UIPOSITIVE, transform.position);
+        optionScreen.SetActive(true);
+        BGM.Select();
+    
+    }
+
+    public void ExitOptions() 
+    {
+        SFX_Manager_HR.instance.PlaySFX(SFX_Manager_HR.SoundEffectNames.UIPOSITIVE, transform.position);
+        foreach (AudioSource item in GameObject.FindObjectsOfType<AudioSource>())
+        {
+            if (item.tag != "BGM")
+            {
+                item.volume = sfxValue;
+            }
+        }
+        optionScreen.SetActive(false);
+        Selectable.Select();
+
+    }
+}
